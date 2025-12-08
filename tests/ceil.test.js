af85136aca38e6f021ead6e16a14a317c27f1097
will-jamm <wiljami.ventila@tuni.fi>
2025-12-08 22:08:02 +0200

    add more unit test cases to increase coverage

--- tests/ceil.test.js
+++ tests/ceil.test.js
@@ -1,25 +1,25 @@
 import ceil from '../src/ceil';
 
-test('Ceil without precision', () => {
+test('should round upwards without precision', () => {
     expect(ceil(5.006)).toBe(6);
     });
 
-test('Ceil with positive precision', () => {
+test('should round upwards to two decimal precision', () => {
     expect(ceil(5.004, 2)).toBe(5.01);
     });
 
-test('Ceil with negative precision', () => {
+test('should round upwards with negative precision', () => {
     expect(ceil(5004, -2)).toBe(5100);
     });
 
-test('Ceil with zero precision', () => {
+test('should round upwards with zero precision', () => {
     expect(ceil(5.6, 0)).toBe(6);
     });
 
-test('ceil with large positive precision', () => {
+test('should round upwards with 6 decimal precision', () => {
     expect(ceil(4.0001234, 6)).toBe(4.000124);
     });
 
-test('ceil with large negative precision', () => {
+test('should round upwards with larger negative precision', () => {
     expect(ceil(412345, -4)).toBe(420000);
     });
--- tests/words.test.js
+++ tests/words.test.js
@@ -1,41 +1,57 @@
 import words from '../src/words';
 
-test('Comma-separated ascii words', () => {
-    expect(words('hiihto, purjehdus, & seura')).toStrictEqual(['hiihto', 'purjehdus', 'seura']);
+test('should split a string separated by comma', () => {
+    expect(words('hiihto, purjehdus, & seura')).toEqual(['hiihto', 'purjehdus', 'seura']);
     });
 
-test('Space seprated ascii words', () => {
-    expect(words('hello world')).toStrictEqual(['hello', 'world']);
+test('should split the string using a provided custom regex pattern', () => {
+    const input = 'älkää, niitä, & hevosia';
+    const pattern = /[^, ]+/g;
+    expect(words(input, pattern)).toEqual(['älkää', 'niitä', '&', 'hevosia']);
 });
 
-test('Multiple spaces & tabs', () => {
-    expect(words('foot\tbar six')).toStrictEqual(['foot', 'bar', 'six']);
+test('should split the string separated by whitespaces', () => {
+    expect(words('hello world')).toEqual(['hello', 'world']);
+});
+
+test('should split thestring with multiple spaces & tabs', () => {
+    expect(words('  foot\tbar six')).toEqual(['foot', 'bar', 'six']);
     });
 
-test('Underscores as delimiters', () => {
-    expect(words('user_name_id')).toStrictEqual(['user', 'name', 'id']);
+test('should split the string with underscores as delimiters', () => {
+    expect(words('user_name_id')).toEqual(['user', 'name', 'id']);
     });
 
-test('NaN string', () => {
-    expect(words('NaN')).toStrictEqual(['Na', 'N']);
+test('should return an empty array for string with custom pattern that matches nothing', () => {
+    expect(words('hello world', /\d+/g)).toEqual([]);
+});
+
+test('should split the string using a provided string pattern', () => {
+    const input = 'hervanta kauppi keskusta';
+    const pattern = ' ';
+    expect(words(input, pattern)).toEqual(['hervanta', 'kauppi', 'keskusta']);
+});
+
+test('should return as is for NaN string', () => {
+    expect(words('NaN')).toEqual(['NaN']);
     });
 
-test('Infinity', () => {
-    expect(words('Infinity')).toStrictEqual(['Infinity']);
+test('should return as is for Infinity string', () => {
+    expect(words('Infinity')).toEqual(['Infinity']);
     });
 
-test('Empty string', () => {
-    expect(words('')).toStrictEqual([]);
+test('should return an empty array for empty string', () => {
+    expect(words('')).toEqual([]);
     });
 
-test('Punctuation marks only', () => {
-    expect(words('.,;:!?&')).toStrictEqual([]);
+test('Should return an empty array for string with punctuation marks', () => {
+    expect(words('.,;:!?&')).toEqual([]);
     });
 
-test('CamelCase string', () => {
-    expect(words('getHTTPResponseCode')).toStrictEqual(['get', 'HTTP', 'Response', 'Code']);
+test('should split CamelCase string', () => {
+    expect(words('getHTTPResponseCode')).toEqual(['get', 'HTTP', 'Response', 'Code']);
     });
 
-test('Numbers separated by spaces', () => {
+test('should split numbers separated by spaces', () => {
     expect(words('123 0456')).toStrictEqual(['123', '0456']);
     });
